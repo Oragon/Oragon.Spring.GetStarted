8a01f3691ff485a202b70105dddadc1138b24a2f
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2019-01-27 01:30:27 -0200

    Versão final para Get Started

--- Oragon.Spring.GetStarted/Controllers/ValuesController.cs
+++ Oragon.Spring.GetStarted/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Oragon.Spring.GetStarted.Model;
 
 namespace Oragon.Spring.GetStarted.Controllers
 {
@@ -12,34 +13,10 @@ namespace Oragon.Spring.GetStarted.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<OragonSpringExample> Get([FromServices] OragonSpringExample oragonSpringExample)
         {
-            return new string[] { "value1", "value2" };
+            return oragonSpringExample;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
---
+++ Oragon.Spring.GetStarted/Model/OragonSpringExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oragon.Spring.GetStarted.Model
+{
+    public class OragonSpringExample
+    {
+        public string StringExample { get; set; }
+
+        public int IntExample { get; set; }
+
+        public DateTime? ReveillonTime { get; set; }
+
+        public DateTime CurrentTime { get; set; }
+
+        public bool BooleanProperty { get; set; }
+
+        public List<OragonSpringExample> ChildList { get; set; }
+
+        public string FillOnConstructor { get; }
+
+        public OragonSpringExample(string constructorParameter)
+        {
+            this.FillOnConstructor = constructorParameter;
+        }
+    }
+}
